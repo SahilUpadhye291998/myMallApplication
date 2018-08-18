4c9b36a1e6f094a3ad556c7ca96a2791ce935973
xGOD666 <sahilsu1998@gmail.com>
2018-08-18 23:08:41 +0530

    This is newer patch Thank you

--- app/src/main/java/Main_Fragment/DashBoardFragment.java
+++ app/src/main/java/Main_Fragment/DashBoardFragment.java
@@ -3,12 +3,22 @@ package Main_Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sahil.mymallapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import DashBoardFragment_Helper.DshBoradTemplate_1;
+import DashBoardFragment_Helper.DshBoradTemplate_2;
 
 
 public class DashBoardFragment extends Fragment {
@@ -16,6 +26,60 @@ public class DashBoardFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard,container,false);
+        View view = inflater.inflate(R.layout.fragment_dashboard,container, false);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) view.findViewById(R.id.result_tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
+        return view;
+
     }
+
+
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new DshBoradTemplate_1(), "Today");
+        adapter.addFragment(new DshBoradTemplate_2(), "Week");
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
